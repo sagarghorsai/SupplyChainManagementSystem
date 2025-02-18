18dfcfd450e8f52e99ab134c64eb7d5ae6c7edc1
sagarghorsai <dostisagar14@gmail.com>
2025-02-18 18:35:34 -0500

    Update

--- CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
+++ CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c433ddf839906c3017184d05676fd0fb441adc81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f9bcebdaa9b31c38b3945e0bdc34fa9f6a3d4dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
