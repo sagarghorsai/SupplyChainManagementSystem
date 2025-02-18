1be14d4924ceeb59bf36890aee4790193a9f5c09
Jsopaseuth <jsopaseuth@davenport.edu>
2025-02-18 13:32:00 -0500

    Add files via upload
    Updated Schema included Customers' table and Employees' table for User authentication.

--- Capstone Team Project DB.sql
+++ Capstone Team Project DB.sql
@@ -1,3 +1,9 @@
+DROP DATABASE IF EXISTS `scdb`;
+CREATE DATABASE `scdb`;
+USE `scdb`;
+
+
+
 /*************************************************************************/
 /*                   SUPPLIER MANAGEMENT                                 */
 /*        Managing relationships with suppliers, procurement, and        */
@@ -37,9 +43,11 @@ CREATE TABLE `Orders` (
     product_id       INT         NOT NULL,
     order_date       DATE,
     quantity_ordered INT         DEFAULT 1,
+    user_id			 INT		 NOT NULL,
     PRIMARY KEY (order_id),
     FOREIGN KEY (supplier_id) REFERENCES Supplier(supplier_id),
-    FOREIGN KEY (product_id)  REFERENCES Product(product_id)
+    FOREIGN KEY (product_id)  REFERENCES Product(product_id),
+    FOREIGN KEY (user_id) 	  REFERENCES Customers(user_id)
 );
 
 
@@ -54,6 +62,42 @@ CREATE TABLE Shipment (
     shipment_date           DATE,
     estimated_arrival_date  DATE,
     actual_arrival_date     DATE,
+    user_id					INT         NOT NULL,
     PRIMARY KEY (shipment_id),
-    FOREIGN KEY (order_id) REFERENCES `Order`(order_id)
+    FOREIGN KEY (order_id) REFERENCES `Orders`(order_id),
+    FOREIGN KEY (user_id) 	  REFERENCES Customers(user_id)
+);
+
+
+/*************************************************************************/
+/*                   CUSTOMER AUTHENTICATION                             */
+/*    Table to hold all customer information and customer id			 */
+/*           to allow customers to create accounts                       */
+/*************************************************************************/
+CREATE TABLE Customers (
+	cust_first				VARCHAR(30) NOT NULL,
+    cust_last				VARCHAR(30) NOT NULL,
+	user_name				VARCHAR(20) NOT NULL,
+    user_password			VARCHAR(50) NOT NULL,
+    user_id					INT         DEFAULT 1,
+	
+    
+    PRIMARY KEY (user_id)
+    
+);
+
+/*************************************************************************/
+/*                   EMPLOYEE AUTHENTICATION                             */
+/*    Employee information such as full name, username, password,        */
+/*           Employee ID, and Administration level (if applicable)       */
+/*************************************************************************/
+
+CREATE TABLE  Employees(
+	employee_first 			VARCHAR(30) NOT NULL,
+    employee_last 			VARCHAR(30) NOT NULL,
+	user_name				VARCHAR(20) NOT NULL,
+    user_password			VARCHAR(50) NOT NULL,
+    employee_id 			INT 		DEFAULT 1,
+    
+    PRIMARY KEY (employee_id)
 );
