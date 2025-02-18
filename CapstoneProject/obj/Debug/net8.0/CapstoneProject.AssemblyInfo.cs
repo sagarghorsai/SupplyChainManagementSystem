6adb1b44e0a523326ec2ec0c33e66ed9bbad95a5
fbennettiv <fredbennettiv@gmail.com>
2025-02-18 15:30:59 -0800

    Initial Basic Layout
    Added initial pages w/o styling

--- CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
+++ CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7f2a9b160c1b8e3c1a51251966b69d3ff71311")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d46f828ddc7c19f5213c5585f9ab894764eaa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
