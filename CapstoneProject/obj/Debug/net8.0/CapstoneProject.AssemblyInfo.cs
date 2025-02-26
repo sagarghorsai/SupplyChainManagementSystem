380e9122d1b28a11260b4501078421a35052e13c
fbennettiv <fredbennettiv@gmail.com>
2025-02-25 22:47:29 -0800

    Navigation Bar Complete
    Changed font and edited navigation bar and its respective icons.

--- CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
+++ CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6adb1b44e0a523326ec2ec0c33e66ed9bbad95a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bdd25d9d5fcffaebaf07e5f25f70ea6e0327425")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CapstoneProject/tailwind.config.js
+++ CapstoneProject/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{razor,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./**/*.{razor,html,js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            sourceSans3: ["SourceSans3"],
+            rubik: ["Rubik"],
+            roboto: ["Roboto"],
+            robotoFlex: ["RobotoFlex"],
+            libreFranklin: ['LibreFranklin'],
+            inter: ['Inter']
+        },
+        extend: {
+        },
+    },
+    plugins: [],
 }
 
