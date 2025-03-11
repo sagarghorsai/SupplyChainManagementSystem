84d1888bb86182b3d3fa010b81e7b47218fdd797
fbennettiv <fredbennettiv@gmail.com>
2025-03-10 20:01:50 -0700

    register updates

--- DataAccessLibrary/obj/Debug/net8.0/DataAccessLibrary.AssemblyInfo.cs
+++ DataAccessLibrary/obj/Debug/net8.0/DataAccessLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54241c758a19a714ae66146046ee4a4e80b25350")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d270a60ea3eeaf368ca5a6461f4435cf5ad83f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
