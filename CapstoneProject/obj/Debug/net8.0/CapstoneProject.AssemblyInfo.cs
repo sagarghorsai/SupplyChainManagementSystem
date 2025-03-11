5acc8d58be9775849ea0b4e8b8257120ee846847
fbennettiv <fredbennettiv@gmail.com>
2025-03-10 19:15:14 -0700

    Nav changes

--- CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
+++ CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7442035bf8bad89138bac3f0c1e9241a0b15578c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea902a98dfcb5b52edc5465fb0ac204b269ff3b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
