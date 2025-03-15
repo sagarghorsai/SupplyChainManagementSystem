ffc262f248eb8e34a0696123c9fdedb21ab1c5b9
fbennettiv <fredbennettiv@gmail.com>
2025-03-15 15:15:10 -0700

    Orders for Employee page 90% done

--- CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
+++ CapstoneProject/obj/Debug/net8.0/CapstoneProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1aba4fa1709383c18ea49fe7b12079f260d48b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc891c4fb1469515a0eb5dd3a5db82012a76e3c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapstoneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
